9063422d7a135a11168efad91da691acd298debb
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-11-05 16:16:25 +0900

    Fix Cylinder mass value + Debugging Cylinder Red color when hit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Slap!");
-        meshRenderer.material.color = meshRenderer.material.color + new Color(0.1f, 0.0f, 0.0f);
+        meshRenderer.material.color = Color.red;
+        StartCoroutine(SlapDebug());
+    }
+    IEnumerator SlapDebug()
+    {
+        yield return new WaitForSeconds(1.0f);
+        meshRenderer.material.color = Color.black ;
     }
 }
